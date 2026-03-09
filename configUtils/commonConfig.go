0a3f831805146d9c45dbaf8279e22abdb3a1f3cf
tiandi <my.english.mail@qq.com>
2026-03-10 00:49:42 +0800

    修改gorm配置

--- configUtils/commonConfig.go
+++ configUtils/commonConfig.go
@@ -76,12 +76,18 @@ type aliYunOss struct {
 }
 
 type gormConfig struct {
-	Url                         string `json:"url"`
-	LogLevel                    string `json:"logLevel"`
-	SlowSqlMillSecond           int    `json:"slowSqlMillSecond"`
-	DisableAutoCreateForeignKey bool   `json:"disableAutoCreateForeignKey"`
-	SingularTable               bool   `json:"singularTable"`
-	MaxOpenConn                 int    `json:"maxOpenConn"`
+	UseUrl                      bool              `json:"useUrl"`
+	Url                         string            `json:"url"`
+	Username                    string            `json:"username"`
+	Password                    string            `json:"password"`
+	Host                        string            `json:"host"`
+	Database                    string            `json:"database"`
+	Param                       map[string]string `json:"param"`
+	LogLevel                    string            `json:"logLevel"`
+	SlowSqlMillSecond           int               `json:"slowSqlMillSecond"`
+	DisableAutoCreateForeignKey bool              `json:"disableAutoCreateForeignKey"`
+	SingularTable               bool              `json:"singularTable"`
+	MaxOpenConn                 int               `json:"maxOpenConn"`
 	//是否翻译错误，比如主键冲突，你想用gorm的DUPLICATE KEY去检查是不行的，必须要先翻译
 	TransError bool `json:"transError"`
 }
--- configUtils/initGorm.go
+++ configUtils/initGorm.go
@@ -5,6 +5,7 @@ import (
 
 	//这是我们自己的包，因为要导入私人包的原因，把GOPRIVATE=设置为了自己的包
 	//但是此时就不走代理了，所以我们要在环境变量里设置一下，让它置空
+	mysqlUtil "github.com/go-sql-driver/mysql"
 	"github.com/nogolang/gorm-zap/gormZap"
 	"go.uber.org/zap"
 	"gorm.io/driver/mysql"
@@ -41,21 +42,34 @@ func getGormConfigCommon(logger *zap.Logger, allConfig *CommonConfig) *gorm.Conf
 }
 
 func SetGormThread(db *gorm.DB, allConfig *CommonConfig) error {
-	rawDb, err := db.DB()
+	raw, err := db.DB()
 	if err != nil {
 		return err
 	}
 
 	//设置最大连接数，需要同时设置数据库本身
-	rawDb.SetMaxOpenConns(allConfig.Gorm.MaxOpenConn)
+	raw.SetMaxOpenConns(allConfig.Gorm.MaxOpenConn)
 	return nil
 }
 
 // NewGormConfig logger由外部注入进来
 func NewGorm(logger *zap.Logger, allConfig *CommonConfig) *gorm.DB {
 	config := getGormConfigCommon(logger, allConfig)
+	finalDns := ""
+	if allConfig.Gorm.UseUrl {
+		finalDns = allConfig.Gorm.Url
+	} else {
+		cfg := mysqlUtil.NewConfig()
+		cfg.DBName = allConfig.Gorm.Database
+		cfg.User = allConfig.Gorm.Username
+		cfg.Passwd = allConfig.Gorm.Password
+		cfg.Addr = allConfig.Gorm.Host
+		cfg.Params = allConfig.Gorm.Param
+		finalDns = cfg.FormatDSN()
+	}
+
 	//gormDb无需使用.session，它Open出来就是一个链式安全的实例
-	db, err := gorm.Open(mysql.Open(allConfig.Gorm.Url), config)
+	db, err := gorm.Open(mysql.Open(finalDns), config)
 	if err != nil {
 		logger.Fatal("gorm连接数据库失败", zap.Error(err))
 		return nil
